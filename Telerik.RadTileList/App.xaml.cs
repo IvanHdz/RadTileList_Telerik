39cd897831f29041e2a8196e2c321307b41e394c
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-07-29 11:28:52 -0500

    Primer commit

---
+++ Telerik.RadTileList/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Telerik.RadTileList
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+           this.InitializeComponent();
+        }
+    }
+}
